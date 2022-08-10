ad017e213b9ee4fb27dea7304ee58c12e1315678
Kyle Deato <kyledeato@gmail.com>
2022-08-10 16:26:37 -0700

    hr w1 diag diff

--- hackerrank/w1/diagonal-differ.js
+++ hackerrank/w1/diagonal-differ.js
@@ -9,39 +9,25 @@ The left-to-right diagonal 1,5,9= 15. The right to left diagonal 3,5,9= 17. Thei
 
 */
 /*
--let left = 0
--let right = 0
--for loop that loops through the whole array
-    -get [0] and add to left
-    -get [2] and add to right
-    -get [4] which is the middle and add to both
-    -get [6] and add to right
-    -get [8] and add to left
 
-- then get the difference of left and right and make it postive for abs value
 */
 function diagonalDifference(arr) {
     // Write your code here
-    let left = 0;
-    let right = 0;
+    // get length of arr
+    let n = arr[0].length
+    let left = 0
+    let right = 0
     
-    let counterLeft = 0;
-    let counterRight = arr[0][0] -1;
 
-    let array = []
-
-    //loop through arr
-    for (let x = 1; x < arr.length; x++){
-            left += arr[x][counterLeft];
-            right += arr[x][counterRight];
-            //console.log("right",arr[x][counterRight -1]);
-            counterLeft++;
-            counterRight--;
-            // console.log(right);
+    for(let x = 0, y = n-1; x< n; x++, y--){
+        //adds left and right then each loop goes down and to inner
+        left += arr[x][x]
+        right += arr[x][y]
     }
-    // console.log(arr[1][1]);
-    // console.log(left);
-    // console.log(right);
-    return Math.abs(left-right)
+    
+    let result = left - right
+    return Math.abs(result)
+
 }
+
 console.log(diagonalDifference([1,2,3,4,5,6,7,8,9]));
